a968acf9b10cc2001978ccd3472e73356e4f5fb4
Mike Vercoelen <mike@frontmen.nl>
2015-02-24 19:10:14 +0100

    forgot these files from last commit

--- extension/scripts/player.js
+++ extension/scripts/player.js
@@ -1,86 +1,133 @@
-var player = {
-  $playPauseBtn: $('#play-pause'),
-  $prevBtn: $('#previous'),
-  $nextBtn: $('#next'),
-  $trackLength: $('#track-length'),
-  $trackCurrentTime: $('#track-current'),
-  isPlaying: false,
-  trackName: null,
-  firstPlay: true,
-  firstNewTrack: true,
-  getTrackName: function () {
-    return $('#track-name a').html();
-  },
-  getTrackLength: function () {
-    return this.$trackLength.html();
-  },
-  getTrackCurrentTime: function () {
-    return this.$trackCurrentTime.html();
-  },
-  getTrackId: function () {
-    var uri = $('#cover-art a').attr('data-itemuri');
-    return uri.replace('spotify:track:', '');
-  },
-  pressPlayPauseBtn: function () {
-    this.$playPauseBtn[0].click();
-  },
-  pressPrevBtn: function () {
-    this.$prevBtn[0].click();
-  },
-  pressNextBtn: function () {
-    this.$nextBtn[0].click();
-  },
-  onPlayPauseBtnChange: function (callback) {
-    var self = this;
+function getPlayPauseBtn () {
+  return $('body').find('#play-pause');
+}
 
-    setInterval(function () {
-      var hasPlayingClass = self.$playPauseBtn.hasClass('playing');
+function getNextBtn () {
+  return $('body').find('#next');
+}
 
-      if (self.isPlaying === false && hasPlayingClass === true) {
-        self.isPlaying = true;
+function getPreviousBtn () {
+  return $('body').find('#previous');
+}
 
-        if (self.firstPlay === true) {
-          self.firstPlay = false;
-          callback(true, true);
-        } else {
-          callback(true, false);
-        }
-      } else if (self.isPlaying === true && hasPlayingClass == false) {
-        self.isPlaying = false;
-        callback(false, false);
-      }
-    }, 16);
-  },
-  onNewTrack: function (callback) {
-    var self = this;
+function getTrackLength () {
+  return $('body').find('#track-length').html();
+}
 
-    setInterval(function () {
+function getCurrentTime () {
+  return $('body').find('#track-current').html();
+}
 
-      // If we aren't playing, there can't be a new track.
-      if (self.isPlaying === false) {
-        return;
-      }
+function getTrackId () {
+  var uri = $('body').find('#cover-art a').attr('data-itemuri');
+  return uri.replace('spotify:track:', '');
+}
 
-      var newTrackName = self.getTrackName();
+function getTrackName () {
+  return $('body').find('#track-name a').html();
+}
 
-      if (self.currentTrackName !== newTrackName) {
-        self.currentTrackName = newTrackName;
-        callback();
-      }
-    }, 16);
-  },
-  onReady: function (callback) {
-    var self = this;
-    var onReadyWatcherInterval = setInterval(onReadyWatcher)
+function isPlaying () {
+  var $btn = getPlayPauseBtn();
+  return $btn.hasClass('playing');
+}
 
-    function onReadyWatcher () {
-      if (self.$playPauseBtn.visible()) {
-        setTimeout(function () {
-          callback();
-        }, 4000);
+function play () {
+  var $btn = getPlayPauseBtn();
 
-        clearInterval(onReadyWatcherInterval);
-      }
+  if (isPlaying() === true) {
+    return;
+  }
+
+  $btn.trigger('click');
+}
+
+function pause () {
+  var $btn = getPlayPauseBtn();
+
+  if (isPlaying() === false) {
+    return;
+  }
+
+  $btn.trigger('click');
+}
+
+function previous () {
+  return getPreviousBtn().trigger('click');
+}
+
+function next () {
+  return getNextBtn().trigger('click');
+}
+
+function onPlayingChange (callback) {
+  var self = this;
+  var lastCheckPlaying = isPlaying();
+
+  setInterval(function () {
+    var currentPlayingStatus = isPlaying();
+
+    if (lastCheckPlaying !== currentPlayingStatus) {
+      lastCheckPlaying = currentPlayingStatus;
+      callback(currentPlayingStatus);
+    }
+  }, 50);
+}
+
+function onTrackTime (time, callback) {
+  var self = this;
+  var interval = setInterval(onTrackTimeInterval, 100);
+
+  function onTrackTimeInterval () {
+    var currentTime = getCurrentTime();
+
+    if (time === currentTime) {
+      clearInterval(interval);
+      return callback();
     }
   }
+}
+
+function onReady (callback) {
+  var self = this;
+  var interval = setInterval(onReadyInterval, 100);
+
+  function onReadyInterval () {
+    if (getPlayPauseBtn().visible()) {
+
+      setTimeout(function () {
+        callback();
+      }, 4000);
+
+      clearInterval(interval);
+    }
+  }
+}
+
+function onNewTrack (callback) {
+  var self = this;
+  var lastTrackName = getTrackName();
+
+  setInterval(function () {
+    if (isPlaying() === true) {
+      var newTrackName = getTrackName();
+
+      if (newTrackName !== lastTrackName) {
+        lastTrackName = newTrackName;
+        callback();
+      }
+    }
+  }, 100);
+}
+
+var player = {
+  onReady: onReady,
+  onTrackTime: onTrackTime,
+  onPlayingChange: onPlayingChange,
+  onNewTrack: onNewTrack,
+  getTrackId: getTrackId,
+  play: play,
+  pause: pause,
+  previous: previous,
+  next: next
 };
